469ea9cb5f16577dcb96867ebd4f061c4b372627
noot <mine@bob.localdomain>
2016-08-06 23:19:59 +0100

    before change to online

--- run.bash
+++ run.bash
@@ -1,6 +1,6 @@
 while :
     do
-        java -jar -Xmx712m craftbukkit.jar
+        java -jar -Xmx712m craftbukkit.jar 2>&1 | tee -a logs/bigLog.txt
         sleep 5
         echo ""
         echo "============================================================"
